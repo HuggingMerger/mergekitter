d66ae60feade8fdce24a1b973b2174c45354d8de
HuggingMerger <droop-dab-unboxed@duck.com>
2026-01-31 05:05:45 +0000

    Update __init__.py

--- mergekit/merge_methods/__init__.py
+++ mergekit/merge_methods/__init__.py
@@ -1,10 +1,14 @@
 # Copyright (C) 2025 Arcee AI
 # SPDX-License-Identifier: LGPL-3.0-only
 
+import mergekit.merge_methods.delerp
 import mergekit.merge_methods.multislerp
 import mergekit.merge_methods.nearswap
-import mergekit.merge_methods.ram
 import mergekit.merge_methods.sce
+import mergekit.merge_methods.wave
+import mergekit.merge_methods.nuslerp
+import mergekit.merge_methods.core_space
+import mergekit.merge_methods.swcm
 from mergekit.merge_methods.base import MergeMethod
 from mergekit.merge_methods.generalized_task_arithmetic import (
     GeneralizedTaskArithmeticMerge,
